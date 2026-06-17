3228a8b96ca5a2ae2bf530bd6aa0bf7d425528df
baileytorch <baileytorch@hotmail.com>
2026-06-17 16:26:07 -0400

    Cargando datos tabla usuarios y publicaciones

--- js/tablitas.js
+++ js/tablitas.js
@@ -3,57 +3,98 @@ const urlPublicaciones = 'https://jsonplaceholder.typicode.com/posts';
 const urlComentarios = 'https://jsonplaceholder.typicode.com/comments';
 const urlTareas = 'https://jsonplaceholder.typicode.com/todos';
 
-function cargarData() {
-    new DataTable('#example', {
-        ajax: {
-            url: 'https://jsonplaceholder.typicode.com/users',
-            dataSrc: ''
-        },
-        columns: [
-            { data: 'id' },
-            { data: 'name' },
-            { data: 'username' },
-            { data: 'email' },
-            { data: 'phone' },
-            { data: 'website' }
-        ],
-        language: window.lenguaje
-    });
+window.onload = function () {
+    $('#divTablaUsuarios').hide();
+    $('#divTablaPublicaciones').hide();
+    $('#divTablaComentarios').hide();
+    $('#divTablaTareas').hide();
 };
 
 function seleccionarData() {
-    const tabla = $('#example');
     const select = $('#selectDatos');
 
     switch (select.val()) {
         case '1':
-            const campos = ['N°', 'Nombre', 'Usuario', 'Correo', 'Teléfono', 'Sitio Web']
-            armarTabla(campos);
-            cargarData();
+            new DataTable('#tablaUsuarios', {
+                ajax: {
+                    url: urlUsuarios,
+                    dataSrc: ''
+                },
+                columns: [
+                    { data: 'id' },
+                    { data: 'name' },
+                    { data: 'username' },
+                    { data: 'email' },
+                    { data: 'phone' },
+                    { data: 'website' }
+                ],
+                language: window.lenguaje
+            });
+            $('#divTablaUsuarios').show();
+            $('#divTablaPublicaciones').hide();
+            $('#divTablaComentarios').hide();
+            $('#divTablaTareas').hide();
             break;
         case '2':
-            alert('Caso 2')
+            new DataTable('#tablaPublicaciones', {
+                ajax: {
+                    url: urlPublicaciones,
+                    dataSrc: ''
+                },
+                columns: [
+                    { data: 'id' },
+                    { data: 'userId' },
+                    { data: 'title' },
+                    { data: 'body' },
+                ],
+                language: window.lenguaje
+            });
+            $('#divTablaUsuarios').hide();
+            $('#divTablaPublicaciones').show();
+            $('#divTablaComentarios').hide();
+            $('#divTablaTareas').hide();
             break;
         case '3':
-            alert('Caso 3')
+            new DataTable('#tablaComentarios', {
+                ajax: {
+                    url: urlComentarios,
+                    dataSrc: ''
+                },
+                columns: [
+                    { data: 'id' },
+                    { data: 'name' },
+                    { data: 'username' },
+                    { data: 'email' },
+                    { data: 'phone' },
+                    { data: 'website' }
+                ],
+                language: window.lenguaje
+            });
+            $('#divTablaUsuarios').hide();
+            $('#divTablaPublicaciones').hide();
+            $('#divTablaComentarios').show();
+            $('#divTablaTareas').hide();
             break;
         case '4':
-            alert('Caso 4')
+            new DataTable('#tablaTareas', {
+                ajax: {
+                    url: urlTareas,
+                    dataSrc: ''
+                },
+                columns: [
+                    { data: 'id' },
+                    { data: 'name' },
+                    { data: 'username' },
+                    { data: 'email' },
+                    { data: 'phone' },
+                    { data: 'website' }
+                ],
+                language: window.lenguaje
+            });
+            $('#divTablaUsuarios').hice();
+            $('#divTablaPublicaciones').hide();
+            $('#divTablaComentarios').hide();
+            $('#divTablaTareas').show();
             break;
     }
-}
-
-function armarTabla(campos) {
-    const tabla = $('#example');
-    let $fila = $('<tr></tr>');
-    let $cabecera = $('<thead></thead>');
-    let $pie = $('<tfoot></tfoot>');
-
-    $.each(campos, function (index, value) {
-        let celda = $(`<th>${value}</th>`);
-        $fila.append(celda);
-    });
-    $cabecera.append($fila);
-    $pie.append($fila);    
-    tabla.append($cabecera, $pie)
 };
